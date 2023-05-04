2e036b611936a0d79431a97a7e0d4f20d7ea87c5
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-05-04 13:18:54 +0300

    deployment

--- whatsappbot/settings.py
+++ whatsappbot/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
     'corsheaders',
 ]
 
