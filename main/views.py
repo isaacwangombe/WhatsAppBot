554e6a93f4673596b0efe1832c7bd16987a58336
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-05-05 06:00:19 +0300

    another

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
+import json
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+
+from .functions import *
 # Create your views here.
 
 
@@ -8,3 +13,22 @@ def welcome(request):
 
 
 
+@csrf_exempt
+def whatsappWebhook(request):
+  if request.method == 'GET':
+    VERIFY_TOKEN = 'test'
+    mode = request.GET['hub.mode']
+    token = request.GET['hub.verify_token']
+    challenge = request.GET['hub.challenge']
+
+    if mode == 'subscribe' and token == VERIFY_TOKEN:
+      return HttpResponse(challenge, status=200)
+    else:
+      return HttpResponse('error', status=403)
+
+
+
+  if request.method == 'POST':
+    data = json.loads(request.body)
+
+    return HttpResponse('success', status=200)
