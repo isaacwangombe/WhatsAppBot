b213880311da8e0281fee05493c32a52c7f3d02e
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-05-09 07:23:47 +0300

    day2

--- main/functions.py
+++ main/functions.py
@@ -1,19 +1,19 @@
 from django.conf import settings
 import requests
 
-# def sendWhatsappMessage(message,phoneNumber):
-#   headers ={"Authorization":settings.WHATSAPP_TOKEN}
-#   payload ={"messaging_product": "whatsapp",
-#             "recipient_type": "individual",
-#             "to": phoneNumber,
-#             "type":"text",
-#             "text":{"body":message}}
-#   response = requests.post(settings.WHATSAPP_URL, headers=headers, json=payload)
-#   ans = response.json()
-#   return ans
+def sendWhatsappMessage(message,phoneNumber):
+  headers ={"Authorization":settings.WHATSAPP_TOKEN}
+  payload ={"messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": phoneNumber,
+            "type":"text",
+            "text":{"body":message}}
+  response = requests.post(settings.WHATSAPP_URL, headers=headers, json=payload)
+  ans = response.json()
+  return ans
 
 
-# phoneNumber = "254706551542"
-# message = "This works!! \n AWESOME \n "
+phoneNumber = "254706551542"
+message = "This works!! \n AWESOME \n "
 
-# ans = sendWhatsappMessage(message, phoneNumber)
+ans = sendWhatsappMessage(message, phoneNumber)
