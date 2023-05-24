8c75c8dd5ffb08a593670358e8c1547256ef6738
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-05-24 07:04:03 +0300

    modifications

--- main/views.py
+++ main/views.py
@@ -10,49 +10,45 @@ from .functions import *
 
 
 def welcome(request):
-   
-    token =settings.WHATSAPP_TOKEN
-    return render(request, 'business/index.html',{'token':token})
 
+    token = settings.WHATSAPP_TOKEN
+    return render(request, 'business/index.html', {'token': token})
 
 
 @csrf_exempt
 def whatsappWebhook(request):
-  if request.method == 'GET':
-    VERIFY_TOKEN = 'test'
-    mode = request.GET['hub.mode']
-    token = request.GET['hub.verify_token']
-    challenge = request.GET['hub.challenge']
+    if request.method == 'GET':
+        VERIFY_TOKEN = 'test'
+        mode = request.GET['hub.mode']
+        token = request.GET['hub.verify_token']
+        challenge = request.GET['hub.challenge']
 
-    if mode == 'subscribe' and token == VERIFY_TOKEN:
-      return HttpResponse(challenge, status=200)
-    else:
-      return HttpResponse('error', status=403)
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            return HttpResponse(challenge, status=200)
+        else:
+            return HttpResponse('error', status=403)
 
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        if 'object' in data and 'entry' in data:
+            if data['object'] == 'whatsapp_business_account':
+                try:
+                    for entry in data['entry']:
+                        phoneNumber = entry['changes'][0]['value']['metadata']['display_phone_number']
+                        phoneId = entry['changes'][0]['value']['metadata']['phone_number_id']
+                        profileName = entry['changes'][0]['value']['contacts'][0]['profile']['name']
+                        whatsAppId = entry['changes'][0]['value']['contacts'][0]['wa_id']
+                        fromId = entry['changes'][0]['value']['messages'][0]['from']
+                        messageId = entry['changes'][0]['value']['messages'][0]['id']
+                        timestamp = entry['changes'][0]['value']['messages'][0]['timestamp']
+                        text = entry['changes'][0]['value']['messages'][0]['text']['body']
 
+                        phoneNumber = '254706551542'
+                        message = 'RE: {} test was received'.format(text)
+                        sendWhatsappMessage(message, phoneNumber)
 
-  if request.method == 'POST':
-    data = json.loads(request.body)
-    if 'object' in data and 'entry' in data:
-      if data['object'] == 'whatsapp_business_account':
-        try:
-          for entry in data['entry']:
-            phoneNumber = entry['changes'][0]['value']['metadata']['display_phone_number']
-            phoneId =  entry['changes'][0]['value']['metadata']['phone_number_id']
-            profileName = entry['changes'][0]['value']['contacts'][0]['profile']['name']
-            whatsAppId = entry['changes'][0]['value']['contacts'][0]['wa_id']
-            fromId = entry['changes'][0]['value']['messages'][0]['from']
-            messageId = entry['changes'][0]['value']['messages'][0]['id']
-            timestamp = entry['changes'][0]['value']['messages'][0]['timestamp']
-            text = entry['changes'][0]['value']['messages'][0]['text']['body']
+                        # handleWhatsappChat(fromId, profileName, phoneId,text)
+                except:
+                    pass
 
-            # phoneNumber = '254706551542'
-            # message = 'RE: {} test was received'.format(text)
-            # sendWhatsappMessage(message, phoneNumber)
-
-            handleWhatsappChat(fromId, profileName, phoneId,text)
-        except:
-          pass
-
-    return HttpResponse( 'success', status=200)
-
+        return HttpResponse('success', status=200)
