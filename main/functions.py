3c6708892a04b7c79165b64e516ce1e6ebb8edcf
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-05-26 07:02:02 +0300

    modifications

--- main/functions.py
+++ main/functions.py
@@ -52,8 +52,8 @@ def handleWhatsappChat(fromId, profileName, phoneId, text):
         # create a chat session
         chat = ChatSession.objects.create(profile=user_profiles)
 
-    # message = 'Welcome to the AI Business Plan creator 😀\n Im gomessage = 'RE: {} test was received'.format(text)ing to take you throught the process of creating your business plan right here on whatsapp\n To get started enter your business name'
-    message = 'RE: {} test was received'.format(text)
+    message = 'Welcome to the AI Business Plan creator 😀\n Im going to take you throught the process of creating your business plan right here on whatsapp\n To get started enter your business name'
+    # message = 'RE: {} test was received'.format(text)
     sendWhatsappMessage(fromId, message)
 
 # continue with function
