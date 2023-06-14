79e32d63665e8dc85e8722b988114b5057b148aa
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-06-14 10:50:22 +0300

    deployment

--- main/aifile.py
+++ main/aifile.py
@@ -1,13 +1,15 @@
 import os
 import openai
 from django.conf import settings
-openai.api_key = settings.OPENAI_API_KEY 
+# openai.api_key = settings.OPENAI_API_KEY
+openai.api_key = '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'
 
 
-def companyDescription(business_name, business_type,country,product_service,short_description,years_operation,progress):
+def companyDescription(business_name, business_type, country, product_service, short_description, years_operation, progress):
     response = openai.Completion.create(
         model="text-davinci-002",
-        prompt=f"Generate Company Description section for a Business Plan for the following business, using the guidelines provided:\nBusiness Name: {business_name}\nBusiness Type:{business_type}\nCountry:{country}\nProduct or Service:{product_service}\nShort Description:{short_description}\n Years in operation:{years_operation} \nProgress to date:{progress}\nstructure, if one is  provided. Write a detailed business description for the short description provided in a professional tone".format(business_name, business_type,country,product_service,short_description,years_operation,progress),
+        prompt=f"Generate Company Description section for a Business Plan for the following business, using the guidelines provided:\nBusiness Name: {business_name}\nBusiness Type:{business_type}\nCountry:{country}\nProduct or Service:{product_service}\nShort Description:{short_description}\n Years in operation:{years_operation} \nProgress to date:{progress}\nstructure, if one is  provided. Write a detailed business description for the short description provided in a professional tone".format(
+            business_name, business_type, country, product_service, short_description, years_operation, progress),
         temperature=0.7,
         max_tokens=2000,
         top_p=1,
@@ -18,9 +20,9 @@ def companyDescription(business_name, business_type,country,product_service,shor
     )
 
     if 'choices' in response:
-        if len(response['choices'])>0:
+        if len(response['choices']) > 0:
             # answer = response['choices'][0]['text']
-            answer = response['choices'][0]['text'].replace('\n',"<br/>")
+            answer = response['choices'][0]['text'].replace('\n', "<br/>")
             return business_name + answer
         else:
             return ""
