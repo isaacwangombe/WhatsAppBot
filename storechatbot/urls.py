d081cf63a8218dff36020093a18c861806f6a7de
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-08-23 11:17:43 +0300

    deploy

--- storechatbot/urls.py
+++ storechatbot/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.welcome, name='test'),
+    path('webhook', views.whatsappWebhook, name='whatsapp-webhook'),
+
 ]
