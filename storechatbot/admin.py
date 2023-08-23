6ad02ae5733fa8b5bfcb97e0c0cbe289d9eb0b44
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-08-23 11:07:25 +0300

    deploy

--- storechatbot/admin.py
+++ storechatbot/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(RepairRequest)
-admin.site.register(Message)
+# admin.site.register(RepairRequest)
+# admin.site.register(Message)
+admin.site.register(Profiles)
+admin.site.register(ChatSession)
