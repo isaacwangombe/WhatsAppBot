e6dfa73c974833163112f2d19cdc150bd5484a49
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-09-12 11:40:27 +0300

    deployment

--- storechatbot/functions.py
+++ storechatbot/functions.py
@@ -34,10 +34,10 @@ def parse_transaction_message(fromId, text):
 
     transaction_code = "Testing"
     amount = 500
-    date = datetime.strptime(
-        ' '.join(datetime.datetime.now()), '%d-%m-%Y %H:%M')
-    recipient_name = "Me"
-    recipient_account = "Mine"
+    # date = datetime.strptime(
+    #     ' '.join(datetime.datetime.now()), '%d-%m-%Y %H:%M')
+    # recipient_name = "Me"
+    # recipient_account = "Mine"
 
     # transaction_code = re.search(
     #     r'(?:Ref\. Number|Transaction ID): ([A-Z0-9]+)', text).group(1)
