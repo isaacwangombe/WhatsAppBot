4a406aea70d64a258be9d89495181ff4d498f77b
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-09-12 11:36:12 +0300

    deployment

--- storechatbot/functions.py
+++ storechatbot/functions.py
@@ -32,12 +32,12 @@ def AreYouDone(fromId):
 def parse_transaction_message(fromId, text):
     # Your regular expression and parsing logic remains the same
 
-    transaction_code = "Testing"
-    amount = 500
-    date = datetime.strptime(
-        ' '.join(datetime.datetime.now()), '%d-%m-%Y %H:%M')
-    recipient_name = "Me"
-    recipient_account = "Mine"
+    # transaction_code = "Testing"
+    # amount = 500
+    # date = datetime.strptime(
+    #     ' '.join(datetime.datetime.now()), '%d-%m-%Y %H:%M')
+    # recipient_name = "Me"
+    # recipient_account = "Mine"
 
     # transaction_code = re.search(
     #     r'(?:Ref\. Number|Transaction ID): ([A-Z0-9]+)', text).group(1)
