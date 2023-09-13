70eea5c1f33744074a81b966be4032db49abc247
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-09-13 06:57:29 +0300

    deployment

--- storechatbot/functions.py
+++ storechatbot/functions.py
@@ -130,14 +130,14 @@ def handleWhatsappChat(fromId, profileName, phoneId, text):
             case "4":
                 chat.chat_purpose = 'complaint'
                 chat.question_no = chat.question_no+1
-                user = User.objects.create_user(
+                User.objects.create_user(
                     username="Name",
                     email='tests@test.com',
                     password='password',
                     first_name="Names",
                 )
                 Profiles.objects.create(
-                    user=user.latest())
+                    user=User.latest())
                 chat.save()
                 message = 'create'
                 sendWhatsappMessage(fromId, message)
