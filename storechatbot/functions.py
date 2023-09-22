0a605dd72f109d0dd36d199abd69eb8e2d89ca4a
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-09-22 12:15:06 +0300

    deployment

--- storechatbot/functions.py
+++ storechatbot/functions.py
@@ -64,7 +64,8 @@ def parse_transaction_message(fromId, text):
     #     # recipient_account="Mine"
     # )
 
-    transaction = Transaction.objects.create(transaction_code=transaction_code)
+    transaction = Transaction.objects.create(
+        transaction_code="transaction_code")
     transaction.save()
 
     # sendWhatsappMessage(fromId, date_str)
