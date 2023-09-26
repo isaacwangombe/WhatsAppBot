c64d68cf72c3b793441dfe3db80c12d50143bf33
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-09-26 12:16:17 +0300

    deployment

--- storechatbot/functions.py
+++ storechatbot/functions.py
@@ -29,9 +29,9 @@ def AreYouDone(fromId):
     sendWhatsappMessage(fromId, message)
 
 
-def renter_payment(fromId, text, transaction):
+def renter_payment(fromId, text):
     if text == "y":
-        sendWhatsappMessage(fromId, transaction.amount)
+        sendWhatsappMessage(fromId, "transaction.amount")
     else:
         sendWhatsappMessage(fromId, "still?")
 
@@ -80,7 +80,7 @@ def parse_transaction_message(fromId, text):
     if transaction:
         message = f"Thank you for uploading the transaction,\n Are these the right transaction details?\n\napartment = {sender.apartment.number} \ntenant = {sender.first_name}\n transaction code = {transaction_code}\n amount = {amount} \n date = {date}\n\n If yes, reply with Y\n if no, reply with N"
         sendWhatsappMessage(fromId, message)
-        renter_payment(fromId, transaction)
+        renter_payment(fromId)
     else:
         sendWhatsappMessage(fromId, "Kindly reupload the message")
 
