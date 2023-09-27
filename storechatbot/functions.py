87e1fa5a28b76dd4001ac00a0b73d4c09553b270
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-09-27 11:48:21 +0300

    deployment

--- storechatbot/functions.py
+++ storechatbot/functions.py
@@ -174,10 +174,10 @@ def handleWhatsappChat(fromId, profileName, phoneId, text):
             )
 
         # create a chat session
-            chat = ChatSession.objects.create(profile=user_profiles)
-            message = 'Welcome to the Apartment Bot 😀\n What would you like to do today?\n\n Please choose any of the following options by typing 1, 2 or 3\n\n1)Send in payment transaction\n2)Get payment details\n3)Request for maintanance'
-            sendWhatsappMessage(fromId, message)
-            return
+        chat = ChatSession.objects.create(profile=user_profiles)
+        message = 'Welcome to the Apartment Bot 😀\n What would you like to do today?\n\n Please choose any of the following options by typing 1, 2 or 3\n\n1)Send in payment transaction\n2)Get payment details\n3)Request for maintanance'
+        sendWhatsappMessage(fromId, message)
+        return
     if chat.question_no == 0:
         match text:
             case "1":
