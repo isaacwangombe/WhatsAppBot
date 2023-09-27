7f55c133dd36813f3e276f53be553307945025d9
Isaac Wangombe <isaacwangombe5@gmail.com>
2023-09-27 17:29:13 +0300

    deployment

--- storechatbot/functions.py
+++ storechatbot/functions.py
@@ -34,11 +34,11 @@ def verifyTransaction(fromId, text):
     if text.upper() == "Y":
         apartment = Profiles.objects.filter(
             phoneNumber=fromId).last().apartment
-        new_balance = apartment.balance - transaction.amount
+        # new_balance = apartment.balance - transaction.amount
 
         # apartment.objects.update(balance=new_balance)
         sendWhatsappMessage(
-            fromId, "Transaction successfully saved"+new_balance)
+            fromId, "Transaction successfully saved"+transaction)
     elif text.upper() == "N":
         transaction.delete()
         sendWhatsappMessage(fromId, "Your upload has been deleted")
